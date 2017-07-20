741b94a687fe5cb3a250ec8275dfdaf55db8ea9d
cheterz <cheterz32@gmail.com>
2017-07-20 08:43:02 +0300

    add main activity

---
+++ app/src/androidTest/java/com/cheterz/remindme/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.cheterz.remindme;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * Created by cheterz on 20.07.2017.
+ */
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/cheterz/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cheterz.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by cheterz on 20.07.2017.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
